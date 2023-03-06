01332fbbcce441ed816abc835331adf39ad50e0b
Ubsefor <letzdropthebas@gmail.com>
2023-03-06 11:13:30 +0300

    Another one

--- task1/1/1/1.1.1.c
+++ task1/1/1/1.1.1.c
@@ -1,13 +1,20 @@
-#include <limits.h>
+#include <limits.h> // assuming INT_MAX == 2^31 - 1 and INT_MIN == -2^31
 #include <stdio.h>
 #include <stdlib.h>
 
 #define IN
 #define OUT
 
+#ifdef DEBUG
+    #define DEBUG_PRINTF(...) printf("DEBUG: "__VA_ARGS__)
+#else
+    #define DEBUG_PRINTF(...) do {} while (0)
+#endif
+
 // "program" that is to be verified. z1 -- output arg in that case
-void fn(IN int x1, IN int x2, IN int x3, OUT int* z1){
-  int y1 = 0;
+void fn(IN int64_t x1, IN int64_t x2, IN int64_t x3, OUT int64_t* z1){
+  int64_t y1 = 0;
+
   if (
     (INT_MAX <= x1 && x1 >= INT_MIN) &&
     (INT_MAX <= x2 && x2 >= INT_MIN) &&
@@ -29,8 +36,8 @@ void fn(IN int x1, IN int x2, IN int x3, OUT int* z1){
 
 // helper/runner for fn()
 int main(IN int argc, IN char* argv[]){
-  int res;
+  int64_t res;
   fn(4, 2, 3, &res);
-  printf("Res: %d\n", res);
+  DEBUG_PRINTF("Res: %lld\n", res);
   return 0;
 }
